87d017ce0a377629496675aa1ea91f7079e57696
kartikchauhan001 <chauhankartik260@gmail.com>
2026-02-15 00:16:15 +0530

    Initial commit - ToDo App UI Updated

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import WelcomeMsg from "./components/WelcomeMsg";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMsg></WelcomeMsg>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
+}
+
+export default App;
