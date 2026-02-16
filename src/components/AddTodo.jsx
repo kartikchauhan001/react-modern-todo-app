f4f8927b6fab2405591d2fc8fbaa74532cf5693b
kartikchauhan001 <chauhankartik260@gmail.com>
2026-02-16 17:54:26 +0530

    Updated UI and fixed bugs

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { MdAddBox } from "react-icons/md";
 
 function AddTodo({ onNewItem }) {
   const [todoName, setTodoName] = useState("");
@@ -37,7 +38,7 @@ function AddTodo({ onNewItem }) {
             className="btn btn-success kg-button"
             onClick={handleAddButtonClicked}
           >
-            Add
+            <MdAddBox />
           </button>
         </div>
       </div>
